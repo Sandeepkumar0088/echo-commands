3a645855daa5c758468ecb690d3055ded316a221
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-12 15:39:33 +0530

    echo commands

--- sample.sh
+++ sample.sh
@@ -1,4 +1,6 @@
 echo print my name
 echo Sandeep kumar
 echo print it in red color
-echo -e \\e[31mSandeep Kumar\\e[0m
+echo -e \\e[31mSandeep Kumar\\e[0m ;
+echo print it in different color
+echo -e \\e[33mSandeep kumar\\e[0m ;
