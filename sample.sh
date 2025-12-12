565bd294620243a14a2d0e91ce87dbaa09c2f19f
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-12 15:31:26 +0530

    echo commands

--- sample.sh
+++ sample.sh
@@ -1,19 +1,4 @@
-echo dowloading nginx
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
-
-echo downloading confi file
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
-cp nginx.conf /etc/nginx/nginx.conf
-
-echo unziping the file
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-systemctl enable nginx
-systemctl start nginx
-
-systemctl restart nginx
+echo print my name
+echo Sandeep kumar
+echo print it in red color
+echo -e \\e[31Sandeep Kumar\\e[0m
